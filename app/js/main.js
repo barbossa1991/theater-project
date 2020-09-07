8cc96b217704cdab2d06d134be48c2730e93e931
Radyslav Dmytrenko <1991barbossa@gmail.com>
2020-09-07 10:26:55 +0300

    make some cosmetic effects for different items

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,37 @@
 $(function () {
 
-    $('.header-slider__inner').slick({
-        nextArrow: '<button type="button" class="slick-btn slick-next"><div class="nav-button"><span>След</span><div class="arrow-next"></div></div></button>',
-        prevArrow: '<button type="button" class="slick-btn slick-prev"><div class="nav-button"><span>Пред</span><div class="arrow-prev"></div></div></button>'
-    });
+  $('.header-slider__inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"><div class="nav-button"><span>След</span><div class="arrow-next"></div></div></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><div class="nav-button"><span>Пред</span><div class="arrow-prev"></div></div></button>'
+  });
 
-    $('.productions__slider').slick({
-        slidesToShow: 4,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 998,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 800,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+  $('.productions__slider').slick({
+    slidesToShow: 4,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 998,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
-    $('.menu-btn').on('click', function () {
-        $('.header-top__list').slideToggle();
-    })
+  $('.menu-btn').on('click', function () {
+    $('.header-top__list').slideToggle();
+  });
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -9,79 +9,79 @@ let gulp = require('gulp'),
 
 
 gulp.task('clean', async function () {
-    del.sync('dist')
+  del.sync('dist');
 });
 
 gulp.task('scss', function () {
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 8 versions']
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({stream: true}))
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 8 versions']
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('css', function () {
-    return gulp.src([
-        'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css'
-    ])
-        .pipe(concat('_libs.scss'))
-        .pipe(gulp.dest('app/scss'))
-        .pipe(browserSync.reload({stream: true}))
+  return gulp.src([
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/slick-carousel/slick/slick.css'
+  ])
+    .pipe(concat('_libs.scss'))
+    .pipe(gulp.dest('app/scss'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('html', function () {
-    return gulp.src('app/*.html')
-        .pipe(browserSync.reload({stream: true}))
+  return gulp.src('app/*.html')
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('script', function () {
-    return gulp.src('app/js/*.js')
-        .pipe(browserSync.reload({stream: true}))
+  return gulp.src('app/js/*.js')
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('js', function () {
-    return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js'
-    ])
-        .pipe(concat('libs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/js'))
-        .pipe(browserSync.reload({stream: true}))
+  return gulp.src([
+    'node_modules/slick-carousel/slick/slick.js'
+  ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: "app/"
+    }
+  });
 });
 
-gulp.task('export', function () {
-    let buildHtml = gulp.src('app/**/*.html')
-        .pipe(gulp.dest('dist'));
+gulp.task('export', async function () {
+  let buildHtml = gulp.src('app/**/*.html')
+    .pipe(gulp.dest('dist'));
 
-    let buildCss = gulp.src('app/css/**/*.css')
-        .pipe(gulp.dest('dist/css'));
+  let buildCss = gulp.src('app/css/**/*.css')
+    .pipe(gulp.dest('dist/css'));
 
-    let buildJs = gulp.src('app/js/**/*.js')
-        .pipe(gulp.dest('dist/js'));
+  let buildJs = gulp.src('app/js/**/*.js')
+    .pipe(gulp.dest('dist/js'));
 
-    let buildFonts = gulp.src('app/fonts/**/*.*')
-        .pipe(gulp.dest('dist/fonts'));
+  let buildFonts = gulp.src('app/fonts/**/*.*')
+    .pipe(gulp.dest('dist/fonts'));
 
-    let buildImg = gulp.src('app/img/**/*.*')
-        .pipe(gulp.dest('dist/img'));
+  let buildImg = gulp.src('app/img/**/*.*')
+    .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
-    gulp.watch('app/*.html', gulp.parallel('html'));
-    gulp.watch('app/js/*.js', gulp.parallel('script'))
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('app/*.html', gulp.parallel('html'));
+  gulp.watch('app/js/*.js', gulp.parallel('script'));
 });
 
 gulp.task('build', gulp.series('clean', 'export'));
